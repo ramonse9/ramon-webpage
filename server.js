b03134a4cd0028d9e5d007f84b37ecb32bb80161
Ramon <ramonantoniojr@gmail.comm>
2019-02-12 13:34:20 -0700

    Tercer commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const hbs = require('hbs');
 require('./hbs/helpers');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/public'));
 
